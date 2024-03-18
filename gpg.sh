8d3fde36661906afd72c90072a99f7a30411f0e2
savasick <savasick@localhost.localdomain>
2024-03-18 21:27:42 +0300

    fix

--- gpg.sh
+++ gpg.sh
@@ -1,10 +1,7 @@
-# Prompt the user to create a new gpg key
 echo "Do you want to create a new gpg key?"
 read -p "Press y/n: " answer
 
-# Check the user's response
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
-    # Generate the gpg key
     gpg --full-generate-key
     gpg --list-keys
     echo "Your new gpg key has been generated and listed."
